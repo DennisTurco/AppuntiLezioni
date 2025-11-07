30251402722be00fb08f0a7e276c7b018cbde452
Dennis Turco <dennisturco@gmail.com>
2025-11-07 18:06:31 +0100

    esercizio veicoli python

---
+++ python/ereditarietà/esercizi/esercizio1.py
@@ -0,0 +1,65 @@
+from datetime import datetime
+
+class Veicolo:
+    def __init__(self, marca, modello, anno, targa):
+        self.marca = marca
+        self.modello = modello
+        self.anno = anno
+        self.targa = targa
+
+    def descrizione(self):
+        return f"Descrizione veicolo: marca: {self.marca}, modello: {self.modello}, anno: {self.anno}, targa: {self.targa}"
+
+    def calcola_costo_manutenzione(self):
+        return int(100)
+
+class Moto(Veicolo):
+    def __init__(self, marca, modello, anno, targa, cilindrata):
+        super().__init__(marca, modello, anno, targa)
+        self.cilindrata = cilindrata
+
+    def calcola_costo_manutenzione(self):
+        return 80 + 5 * datetime.now().year - self.anno
+
+    def descrizione(self):
+        return super().descrizione() + f", cilindrata: {self.cilindrata}"
+
+class Camion(Veicolo):
+    def __init__(self, marca, modello, anno, targa, portata_massima):
+        super().__init__(marca, modello, anno, targa)
+        self.portata_massima = portata_massima
+
+    def calcola_costo_manutenzione(self):
+        return 200 + 20 * datetime.now().year - self.anno
+
+    def descrizione(self):
+        return super().descrizione() + f", portata massima: {self.portata_massima}"
+
+class Auto(Veicolo):
+    def __init__(self, marca, modello, anno, targa, tipo_carburante, numero_porte):
+        super().__init__(marca, modello, anno, targa)
+        self.tipo_carburante = tipo_carburante
+        self.numero_porte = numero_porte
+
+    def calcola_costo_manutenzione(self):
+        return 100 + 10 * datetime.now().year - self.anno
+
+    def descrizione(self):
+        return super().descrizione() + f", carburante: {self.tipo_carburante}, porte: {self.numero_porte}"
+
+class ParcoVeicoli:
+    def __init__(self):
+        self.veicoli = []
+
+    def aggiungi_veicolo(self, veicolo):
+        self.veicoli.append(veicolo)
+
+    def stampa_veicoli(self):
+        for veicolo in self.veicoli:
+            print(veicolo.descrizione())
+
+    def costo_totale_manutenzione(self):
+        somma = 0
+        for veicolo in self.veicoli:
+            somma += veicolo.calcola_costo_manutenzione()
+        return somma
