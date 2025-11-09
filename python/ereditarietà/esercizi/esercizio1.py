7e5355bf7ecf4f6520b2f70680f505aebd384ce3
Dennis Turco <dennisturco@gmail.com>
2025-11-09 12:07:01 +0100

    eccezioni python

--- python/ereditarietà/esercizi/esercizio1.py
+++ python/ereditarietà/esercizi/esercizio1.py
@@ -1,6 +1,9 @@
+# Testo esercizio: https://appuntifacili.it/informatica/python/15_ereditarieta#102-esercizio--gestione-di-un-parco-veicoli-aziendale
+
+from abc import ABC
 from datetime import datetime
 
-class Veicolo:
+class Veicolo(ABC):
     def __init__(self, marca, modello, anno, targa):
         self.marca = marca
         self.modello = modello
@@ -47,6 +50,8 @@ class Auto(Veicolo):
     def descrizione(self):
         return super().descrizione() + f", carburante: {self.tipo_carburante}, porte: {self.numero_porte}"
 
+
+#PARCO VEICOLI ________________________
 class ParcoVeicoli:
     def __init__(self):
         self.veicoli = []
@@ -63,3 +68,42 @@ class ParcoVeicoli:
         for veicolo in self.veicoli:
             somma += veicolo.calcola_costo_manutenzione()
         return somma
+
+
+if __name__ == "__main__":
+
+    parcheggio = ParcoVeicoli()
+
+    macchina_rossa=Auto("Lancia", "Y", 2012, "BBBB","diesel", 3)
+    camion_blu=Camion("Iveco", "Leoncino", 1998, "CCCC", 3000)
+    scooter_nero=Moto("Piaggio","Liberty",2010,"AAAAA",50)
+
+    parcheggio.aggiungi_veicolo(scooter_nero)
+    parcheggio.aggiungi_veicolo(macchina_rossa)
+    parcheggio.aggiungi_veicolo(camion_blu)
+
+    # abbiamo parcheggiato le nostre macchine nel parcheggio. vediamo quante ce ne sono
+
+    parcheggio.stampa_veicoli()
+
+    '''
+    Anzichè stampare i costi singolarmente: non tanto bello -> se avessimo 1000 auto, ci mettiamo a mettere 1000 chiamate con 1000 print??
+    Meglio usare un for...
+
+        costo1 = macchina_rossa.calcola_costo_manutenzione()
+        print(f"Costo manutenzione: {costo1}")
+
+        costo2 = scooter_nero.calcola_costo_manutenzione()
+        print(f"Costo manutenzione: {costo2}")
+
+        costo3 = camion_blu.calcola_costo_manutenzione()
+        print(f"Costo manutenzione: {costo3}")
+    '''
+
+    for i in range(0, len(parcheggio.veicoli)): # iterando sui veicoli
+        costo = parcheggio.veicoli[i].calcola_costo_manutenzione()
+        print(f"Costo manutenzione: {costo}")
+
+    # per ottenere il costo totale di tutte le auto nel parcheggio
+    costo_totale = parcheggio.costo_totale_manutenzione()
+    print(f"Costo Totale: {costo_totale}")
---
+++ python/gestione_eccezioni/gestione_eccezioni.py
@@ -0,0 +1,20 @@
+
+n = int(input("Numeratore: "))
+d = int(input("Denominatore: "))
+
+lista = [1, 2, 3]
+
+try:
+    # nell'ipotesi che d == 0, questa istruzione genererà un eccezione: ZeroDivisionError: division by zero
+    risultato = n / d
+    print(f"Risultato = {risultato}")
+
+    # se la lista ha solo 3 elementi, non esisterà la posizione 5 -> genera IndexError
+    print(lista[5])
+except ZeroDivisionError:
+    print("impossibile eseguire la divisione")
+except IndexError:
+    print("posizione lista non valida")
+
+risultato2 = n * d
+print(f"Risultato = {risultato2}")
