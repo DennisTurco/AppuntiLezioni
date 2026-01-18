b0027f427048b0664c638e8525083b1bb085d40e
DennisTurco <dennisturco@gmail.com>
2026-01-18 21:06:20 +0100

    esempi liste

---
+++ python/esempi_corso_start2data/list.py
@@ -0,0 +1,24 @@
+lista = [] # dichiarazione lista vuota
+
+for i in range(10):
+    lista.append(i) # metodo per aggiungere elementi nella lista
+
+print(lista)
+
+# aggiungo valore nel mezzo
+lista.insert(5, 0)
+
+print(lista)
+
+lista2 = [0, 0, 0]
+
+# estendere lista con altra lista
+
+lista.extend(lista2)
+
+print(lista)
+
+# ordinamento
+
+print(sorted(lista))
+print(sorted(lista, reverse=True))
---
+++ python/esempi_corso_start2data/list_esercizio.py
@@ -0,0 +1,36 @@
+# data una lista vuota e una dimensione n definita inizialmente, vogliamo che vengano randomicizzati
+# n elementi nella lista con random, si ricerca il valore massimo e si ordina
+# (senza usare le operazioni delle liste, quindi con cicli a vecchia scuola)
+
+import random
+
+numeri = []
+dimensione = 10
+
+# polamento randomico
+for i in range(dimensione):
+    valore_random = random.randint(0, 99)
+    numeri.append(valore_random)
+
+print(numeri)
+
+# ricerca valore massimo
+# logica: https://appuntifacili.it/informatica/cpp/4_array-monodimensionali#54-ricerca-massimominimo
+max = numeri[0] # max parte dal primo elemento della lista
+for i in range(len(numeri)): # va bene anche se si mette 'dimensione'
+    if numeri[i] > max:
+        max = numeri[i]
+
+print(f"Il massimo è: {max}")
+
+# ordinamento con bubble sort
+# logica: https://appuntifacili.it/informatica/cpp/4_array-monodimensionali#53-ordinamento-array-bubble-sort
+for i in range(len(numeri)):
+    for j in range(len(numeri) - 1):
+        if numeri[j] > numeri[j + 1]:
+            # logica di swap per lo scambio: https://appuntifacili.it/informatica/cpp/4_array-monodimensionali#53-ordinamento-array-bubble-sort
+            temp = numeri[j]
+            numeri[j] = numeri[j + 1]
+            numeri[j + 1] = temp
+
+print(numeri)
